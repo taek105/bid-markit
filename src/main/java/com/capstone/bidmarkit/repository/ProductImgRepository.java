ac41d551461e549ccca3b8429fd1a782cf294b14
taek105 <ht5172@gmail.com>
2024-05-22 22:34:18 +0900

    쿼리 추가

--- src/main/java/com/capstone/bidmarkit/repository/ProductImgRepository.java
+++ src/main/java/com/capstone/bidmarkit/repository/ProductImgRepository.java
@@ -2,8 +2,16 @@ package com.capstone.bidmarkit.repository;
 
 import com.capstone.bidmarkit.domain.ProductImg;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 public interface ProductImgRepository extends JpaRepository<ProductImg, Integer>{
     List<ProductImg> findByProductId(int productId);
+
+//    @Query("SELECT p.imgUrl FROM ProductImg p WHERE p.product.Id = :productId")
+//    String findImgUrlsByProductId(@Param("productId") int productId);
+
+    @Query("SELECT p.imgUrl FROM ProductImg p WHERE p.product.id = :productId")
+    List<String> findImgUrlsByProductId(@Param("productId") int productId);
 }
