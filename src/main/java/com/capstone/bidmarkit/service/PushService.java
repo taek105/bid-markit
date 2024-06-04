c11ff8a0fc0172a6f023812a8baa8c7e1aa8b3ca
sjsk3232 <sjsk3232@gmail.com>
2024-06-04 18:44:21 +0900

    `24.06.04. update

--- src/main/java/com/capstone/bidmarkit/service/PushService.java
+++ src/main/java/com/capstone/bidmarkit/service/PushService.java
@@ -5,14 +5,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @RequiredArgsConstructor
 @Service
 public class PushService {
 
     private final RestTemplate restTemplate;
-    private final WebClient webClient;
 
     @Value("${push.server.url}")
     private String apiUrl; // 알림 서버의 URL
@@ -21,14 +19,6 @@ public class PushService {
     public void pushAlarm (PushAlarmRequest request) {
         try {
             restTemplate.postForObject(apiUrl + "/push/" + request.getMemberId(), request, Void.class);
-//            webClient.get().uri(uriBuilder -> uriBuilder
-//                    .path("/push/" + request.getMemberId())
-//                    .queryParam("productName", request.getProductName())
-//                    .queryParam("imgURL", request.getImgURL())
-//                    .queryParam("content", request.getContent())
-//                    .queryParam("type", request.getType())
-//                    .build()
-//            ).retrieve().bodyToMono(Void.class);
         } catch (Exception e) {
             System.out.println(e);
             throw new RuntimeException("error in pushalarm");
